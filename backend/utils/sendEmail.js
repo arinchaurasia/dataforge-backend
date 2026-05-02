29c61241384f06ec6f0391067d239240ef73296a
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-02 09:25:19 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,27 +1,27 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    family: 4, // Force IPv4
-    auth: {
-      user: process.env.EMAIL_USER, 
-      pass: process.env.EMAIL_PASS  
-    }
-  });
-
-  const mailOptions = {
-    from: '"DataForge Pro Security" <no-reply@dataforge.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message
-  };
-
   try {
-    await transporter.sendMail(mailOptions);
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS  
+      }
+    });
+
+    const mailOptions = {
+      from: '"DataForge Pro Security" <no-reply@dataforge.com>',
+      to: options.email,
+      subject: options.subject,
+      text: options.message
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email successfully sent: " + info.response);
+
   } catch (error) {
-    console.error('FULL NODEMAILER ERROR:', error);
-    throw new Error(`Email failed: ${error.message}`);
+    console.error("CRITICAL EMAIL ERROR: ", error);
   }
 };
 
