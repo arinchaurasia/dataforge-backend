d2f6a676464cebe7bf8ce2c995a4c8c5b231cb9e
Arin Chaurasia <atulchaurasia39@gmail.com>
2026-05-02 08:51:25 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -4,7 +4,8 @@ const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true,
+    family: 4, // Force IPv4
     auth: {
       user: process.env.EMAIL_USER, 
       pass: process.env.EMAIL_PASS  
